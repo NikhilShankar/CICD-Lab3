ae5ac7ffce34243ac39ca1563bedb76b48c136a2
nixacfilms <nikhil.nixaclabs@gmail.com>
2025-11-26 23:44:46 -0500

    Corrected unnecessary extra folder

--- lambda/index.py
+++ lambda/index.py
@@ -5,7 +5,7 @@ def handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'Hello from Lambda! After triggering redeployment',
+            'message': 'Hello from Lambda! After triggering redeployment. Trying for second time',
             'timestamp': datetime.now().isoformat()
         })
     }
